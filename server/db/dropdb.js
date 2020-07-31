19891609f0a850d941f536a8c8d215e9eb544c73
AZGchip <andrew@zinski.net>
2020-07-31 17:52:49 -0400

    added function to remove user collection

--- server/db/dropdb.js
+++ server/db/dropdb.js
@@ -1,5 +1,5 @@
 const db = require('../config/connection');
-db.db.dropCollection('users', function(err, result) {
+db.dropCollection('users', function(err, result) {
     if (err){console.log(err)}
     else{ console.log("user data wiped successfully",result)}
 });
