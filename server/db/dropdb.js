6018010c19f43cc4bf410b6fb713b5ae748f8e2e
AZGchip <andrew@zinski.net>
2020-07-31 17:48:07 -0400

    fixed dropdb connection route

--- server/db/dropdb.js
+++ server/db/dropdb.js
@@ -1,4 +1,4 @@
-const db = require('./config/connection');
+const db = require('../config/connection');
 db.db.dropCollection('users', function(err, result) {
     if (err){console.log(err)}
     else{ console.log("user data wiped successfully",result)}
